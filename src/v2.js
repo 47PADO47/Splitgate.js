bbb039ca17117081a68a2bdade348739910e8628
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2022-06-03 22:30:02 +0200

    ✨
    - New methods
    - Login via refreshToken

--- src/v2.js
+++ src/v2.js
@@ -3,6 +3,7 @@ const BaseApi = require('./Base');
 class v2 extends BaseApi {
     #platformToken = "";
     #token = "";
+    #refreshToken = "";
     constructor(data = {
         platformToken: "",
         debug: false,
@@ -11,6 +12,7 @@ class v2 extends BaseApi {
         this.#platformToken = data.platformToken;
 
         this.#token = "";
+        this.#refreshToken = "";
         this.authorized = false;
 
         this.user = {
@@ -36,9 +38,9 @@ class v2 extends BaseApi {
                 "Authorization": `Basic MzZkOGEzN2JmZTlhNDEyYWJiMGIzMTc0OTM0NTg5YjU6`,
                 ...this.headers,
             },
-            body: `platform_token=${platformToken}`,
+            body: this.#refreshToken ? `grant_type=refresh_token&refresh_token=${this.#refreshToken}` : `platform_token=${platformToken}`,
         });
-        if (response.status !== 200) return this.error(`Server returned a different status (${response.status} - ${response.statusText})`);
+        if (response.status !== 200) return this.error(`Server status different from 200 (${response.status} - ${response.statusText})`);
         
         const json = await response.json()
         .catch(() => {
@@ -47,7 +49,9 @@ class v2 extends BaseApi {
         
         if (json.error) return this.error(json.error_description);
 
+        this.#platformToken = platformToken;
         this.#token = json.access_token;
+        this.#refreshToken = json.refresh_token;
         this.user = {
             name: json.display_name,
             platform: {
@@ -62,10 +66,8 @@ class v2 extends BaseApi {
 
         setTimeout(() => {
             this.authorized = false;
-            this.#token = "";
-
-            this.log("Logged out (Token expired)");
-        }, json.expires_in*1000);
+            this.login(this.#platformToken);
+        }, json.expires_in*60*1000);
         return this.user;
     };
 
@@ -77,8 +79,8 @@ class v2 extends BaseApi {
         return this.authorized;
     };
 
-    async getAccountCosmetics() {
-        const data = await this.#fetch(`platform/public/namespaces/splitgate/users/${this.user.id}/customizations/chosen`);
+    async getCosmetics(userId = this.user.id) {
+        const data = await this.#fetch(`platform/public/namespaces/splitgate/users/${userId}/customizations/chosen`);
         return data ?? {};
     };
 
@@ -92,14 +94,9 @@ class v2 extends BaseApi {
         return data ?? [];
     };
 
-    async claimDailyReward() {
-        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/users/${this.user.id}/dailyCheckIn/status`);
-        return data ?? {};
-    };
-
     async getBlocks() {
         const data = await this.#fetch(`lobby/v1/public/player/namespaces/splitgate/users/me/blocked`);
-        return data?.data ?? [];
+        return data ?? [];
     };
 
     async getProfile() {
@@ -109,9 +106,18 @@ class v2 extends BaseApi {
 
     async getUserProfiles(userIds = [""]) {
         const data = await this.#fetch(`iam/v3/public/namespaces/splitgate/users/bulk/basic`, {
-            body: JSON.stringify({userIds})
+            body: JSON.stringify({userIds}),
+            method: "POST",
         });
-        return data?.data ?? {};
+        return data ?? {};
+    };
+
+    async getUserProfilesSteam(platformUserIds = [""]) {
+        const data = await this.#fetch(`iam/v3/public/namespaces/splitgate/platforms/steam/users`, {
+            body: JSON.stringify({platformUserIds}),
+            method: "POST",
+        });
+        return data ?? {};
     };
 
     async getReferralData() {
@@ -119,13 +125,182 @@ class v2 extends BaseApi {
         return data ?? {};
     };
 
-    async getReferralSeasonData(seasonName = "Season1") {
+    async getReferralSeasonData(seasonName = "Season2") {
         const data = await this.#fetch(`social/public/namespaces/splitgate/users/${this.user.id}/referral/data?seasonName=${seasonName}`);
         return data ?? {};
     };
 
     async getSeasonPass(lang = "en") {
         const data = await this.#fetch(`seasonpass/public/namespaces/splitgate/seasons/current?language=${lang}`);
+        return data ?? {};
+    };
+
+    async getSeasonReward() {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/users/${this.user.id}/seasonreward`);
+        return data ?? {};
+    };
+
+    async getPublicProfiles(userIds = [""]) {
+        const data = await this.#fetch(`basic/v1/public/namespaces/splitgate/profiles/public?userIds=${userIds.join(",")}`);
+        return data ?? {};
+    };
+
+    async getStreamStatus() {
+        const data = await this.#fetch(`basic/public/namespaces/splitgate/streamStatus`);
+        return data ?? {};
+    };
+
+    async getServers() {
+        const data = await this.#fetch(`qosm/public/qos`);
+        return data ?? {};  
+    };
+
+    async getFeedStatus() {
+        const data = await this.#fetch(`basic/public/namespaces/splitgate/feedStatus`);
+        return data ?? {};
+    };
+
+    async getChallenges() {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/challenges`);
+        return data ?? {};
+    };
+
+    async getChallengesState() {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/challenges/state`);
+        return data ?? {};
+    };
+
+    async getCurrentSeasonName() {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/seasons/current/name`);
+        return data ?? {};
+    };
+
+    async getPlacementGamesNeeded() {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/stats/users/placementGamesNeeded`);
+        return data ?? {};
+    };
+
+    async getViews() {
+        const data = await this.#fetch(`platform/public/namespaces/splitgate/views`);
+        return data ?? {};
+    };
+
+    async getViewSections(viewId = "") {
+        const data = await this.#fetch(`platform/public/namespaces/splitgate/views/${viewId}/sections`);
+        return data ?? {};
+    };
+
+    async getItems(itemIds=[""]) {
+        const data = await this.#fetch(`/platform/public/namespaces/splitgate/items/locale/byIds?itemIds=${itemIds.join(",")}`);
+        return data ?? {};
+    };
+
+    async getStats(userIds = [this.user.id]) {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/stats/users/account?userIds=${userIds.join(",")}`);
+        return data ?? {};
+    };
+
+    async getRaceTimes(userId = this.user.id, platform = "STEAM") {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/users/${userId}/race?platform=${platform.toUpperCase()}`);
+        return data ?? {};
+    };
+
+    async getDailyPlayStreak(userId = this.user.id) {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/users/${userId}/dailyPlayStreak`);
+        return data ?? {};
+    };
+
+    async getDailyCheckInStatus(userId = this.user.id) {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/users/${userId}/dailyCheckIn/status`);
+        return data ?? {};
+    };
+
+    async SyncSteamDlc(userId = this.user.id, steamId = "", appId = "677620") {
+        const data = await this.#fetch(`platform/public/namespaces/splitgate/users/${userId}/dlc/steam/sync`, {
+            body: JSON.stringify({steamId, appId}),
+            method: "PUT"
+        });
+        return data ?? {};
+    };
+
+    async SyncSteamIap(userId = this.user.id, steamId = "", appId = "677620") {
+        const data = await this.#fetch(`platform/public/namespaces/splitgate/users/${userId}/iap/steam/sync`, {
+            body: JSON.stringify({steamId, appId}),
+            method: "PUT"
+        });
+        return data ?? {};
+    };
+
+    async getBadges(userIds = [this.user.id]) {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/badges/users?userIds=${userIds.join(",")}`);
+        return data ?? {};
+    };
+
+    async getDrops(userId = this.user.id) {
+        const data = await this.#fetch(`platform/public/namespaces/splitgate/users/${userId}/drops`);
+        return data ?? {};
+    };
+
+    async getCurrentSeasonUserData(userId = this.user.id) {
+        const data = await this.#fetch(`/seasonpass/public/namespaces/splitgate/users/${userId}/seasons/current/data`);
+        return data ?? {};
+    };
+
+    async getLegacyProgression(userId = this.user.id) {
+        const data = await this.#fetch(`social/public/namespaces/splitgate/users/${userId}/progression/legacy`);
+        return data ?? {};
+    };
+
+    async getProgression(userId = this.user.id) {
+        const data = await this.#fetch(`social/public/namespaces/splitgate/users/${userId}/progression`);
+        return data ?? {};
+    };
+
+    async getWallet() {
+        const data = await this.#fetch(`platform/public/namespaces/splitgate/users/${this.user.id}/wallets/SC`);
+        return data ?? {};
+    };
+
+    async getParty(partyId = "") {
+        const data = await this.#fetch(`lobby/v1/public/party/namespaces/splitgate/parties/${partyId}`);
+        return data ?? {};
+    };
+
+    async claimDailyReward() {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/users/${this.user.id}/dailyreward/claim`);
+        return data ?? {};
+    };
+
+    async redeemCode(code = "", region = "US", lang = "en") {
+        const data = await this.#fetch(`platform/public/namespaces/splitgate/users/${this.user.id}/fulfillment/code`, {
+            body: JSON.stringify({code, region, lang}),
+            method: "POST"
+        });
+        return data ?? {};
+    };
+
+    async getMatch(matchId = "") {
+        const data = await this.#fetch(`sessionmanager/namespaces/splitgate/matchsession/${matchId}`);
+        return data ?? {};
+    };
+
+    async rejectMatch(matchId = "", ticket = "") {
+        const data = await this.#fetch(`sessionmanager/namespaces/splitgate/gamesession/${matchId}/ticket/${ticket}/reject-match`, {
+            method: "DELETE"
+        });
+        return data ?? {};
+    };
+
+    async getUserSessions(userId = this.user.id) {
+        const data = await this.#fetch(`sessionmanager/namespaces/splitgate/users/${userId}/sessions`);
+        return data ?? {};
+    };
+
+    async claimChallengeReward(challengeId = "", challengeType = "") {
+        const data = await this.#fetch(`splitgate/public/namespaces/splitgate/users/${this.user.id}/challenges/claim-reward`, {
+            body: JSON.stringify({challengeType, challengeId}),
+            method: "POST"
+        });
         return data ?? {};
     };
 
@@ -140,7 +315,7 @@ class v2 extends BaseApi {
             }
         });
 
-        if (response.status !== 200) return this.error(`Server returned a different status (${response.status} - ${response.statusText})`);
+        if (response.status !== 200) return this.error(`Server status different from 200 (${response.status} - ${response.statusText})`);
 
         const json = await response.json()
         .catch(() => {
